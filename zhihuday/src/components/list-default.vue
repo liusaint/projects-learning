fb5a348622bafd0b8063e4316a3c7f16bdc5b601
Runner <841766635@qq.com>
2017-06-14 23:20:42 +0800

    轮播

--- zhihuday/src/components/list-default.vue
+++ zhihuday/src/components/list-default.vue
@@ -1,4 +1,87 @@
 <template>
-	
-	123
+	<!--轮播组件-->
+	<slider :top_stories="topStories" v-cloak></slider>
+
 </template>
+
+<script>
+	
+	import ajax from '../ajax'
+	import slider from '../components/slider.vue'
+
+	export default{
+		components:{
+			slider
+		},
+		data () {return {
+			topStories: [],
+			allStories: [],
+			date: '',
+			loading: false,
+			dateArr: [],
+			titleTip: ''
+		}
+	},
+	ready () {
+		this.getLatest()
+	},
+	route: {
+		data (transition) {
+			var _this = this
+			if (transition.from.name === 'detail') {
+				_this.$nextTick(function () {
+					window.document.body.scrollTop = window.sessionStorage.scrollTop
+				})
+			} else {
+//          _this.getLatest()
+window.document.body.scrollTop = 0
+}
+window.addEventListener('scroll', _this.getScrollList, false)
+window.addEventListener('touchmove', _this.whatsTitle, false)
+transition.next()
+},
+deactivate (transition) {
+	var _this = this
+	window.removeEventListener('scroll', _this.getScrollList, false)
+	window.removeEventListener('touchmove', _this.whatsTitle, false)
+	window.sessionStorage.scrollTop = window.document.body.scrollTop
+	transition.next()
+}
+},
+methods:{
+	getLatest () {
+		let _this = this
+		_this.loading = true
+		ajax({
+//        url: 'http://news-at.zhihu.com/api/4/news/latest',
+url: '/news-at/api/4/news/latest',
+method: 'GET',
+callback: function (res) {
+	_this.$set('topStories', res.top_stories)
+	_this.$set('allStories', _this.allStories.concat(res))
+	_this.$set('date', res.date)
+	_this.dateArr.push(_this.date)
+	_this.loading = false
+}
+})
+	},
+}
+}
+
+
+</script>
+
+
+<style scoped lang="scss" rel="stylesheet/scss">
+	.list-box{
+		padding: 10px 5px 0 5px;
+		background: #f2f2f2;
+	}
+	.title{
+		color: #76787e;
+		margin-left: 15px;
+		margin-bottom: 12px;
+		font-size: 14px;
+		font-weight: 500;
+	}
+</style>
